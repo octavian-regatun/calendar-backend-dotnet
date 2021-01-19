60c7fb4eea4b28c034f08a0bd4a54f7007202422
Octavian Regatun <octavian.regatun@outlook.com>
2021-01-19 10:40:37 +0200

    feat: ✨ add LoggedIn filter

--- Auth/Authentication.cs
+++ Auth/Authentication.cs
@@ -1,25 +1,25 @@
+using System.Net;
 using calendar_backend_dotnet.Entities;
 using calendar_backend_dotnet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using MongoDB.Driver;
 
 namespace calendar_backend_dotnet.Auth
 {
     public static class AuthService
     {
-        public static class Providers
+        public class LoggedInFilter : ActionFilterAttribute
         {
-            public static string Google = "google";
-            public static string Facebook = "facebook";
-            public static string Twitter = "twitter";
-            public static string Apple = "apple";
-            public static string GitHub = "github";
-        }
+            public override void OnActionExecuting(ActionExecutingContext context)
+            {
+                var sessionToken = context.HttpContext.Request.Cookies["session"];
 
-        public static class Roles
-        {
-            public static string User = "U";
-            public static string Free = "F";
-            public static string Admin = "A";
+                if (IsLoggedIn(sessionToken))
+                {
+                    context.Result = new UnauthorizedResult();
+                }
+            }
         }
 
         public static bool IsLoggedIn(string sessionId)
@@ -39,7 +39,20 @@ namespace calendar_backend_dotnet.Auth
 
             return false;
         }
+        public static class Providers
+        {
+            public static string Google = "google";
+            public static string Facebook = "facebook";
+            public static string Twitter = "twitter";
+            public static string Apple = "apple";
+            public static string GitHub = "github";
+        }
+
+        public static class Roles
+        {
+            public static string User = "U";
+            public static string Free = "F";
+            public static string Admin = "A";
+        }
     }
-
-
 }
