0f1b21bf363a14075d8df73dc6d94bd64116f9d2
Octavian Regatun <octavian.regatun@outlook.com>
2021-01-19 10:43:51 +0200

    refactor: ♻ refactor Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,23 +1,11 @@
-using System.ComponentModel.Design;
 using System.IO;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using calendar_backend_dotnet.Entities;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace calendar_backend_dotnet
 {
@@ -37,11 +25,6 @@ namespace calendar_backend_dotnet
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
-            // services.AddSwaggerGen(c =>
-            // {
-            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "calendar_backend_dotnet", Version = "v1" });
-            // });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -49,9 +32,6 @@ namespace calendar_backend_dotnet
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-
-                // app.UseSwagger();
-                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "calendar_backend_dotnet v1"));
             }
 
             if (IsDevelopment)
@@ -66,10 +46,6 @@ namespace calendar_backend_dotnet
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
-            app.UseAuthorization();
-
-            // app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
